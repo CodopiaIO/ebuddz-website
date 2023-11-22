0422a397a7ee6bdba42fe331416e67980ef2f511
Abed El Rahman Al Tawil <63475475+mrtawil@users.noreply.github.com>
2023-11-22 16:40:35 +0000

    add v2 website

---
+++ js/script.js
@@ -0,0 +1,293 @@
+"use strict";
+(function () {
+	// Global variables
+	var
+		userAgent = navigator.userAgent.toLowerCase(),
+		initialDate = new Date(),
+
+		$document = $(document),
+		$window = $(window),
+		$html = $("html"),
+		$body = $("body"),
+
+		isDesktop = $html.hasClass("desktop"),
+		isIE = userAgent.indexOf("msie") !== -1 ? parseInt(userAgent.split("msie")[1], 10) : userAgent.indexOf("trident") !== -1 ? 11 : userAgent.indexOf("edge") !== -1 ? 12 : false,
+		isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),
+		windowReady = false,
+		isNoviBuilder = false,
+		livedemo = true,
+
+		plugins = {
+			owl: $('.owl-carousel'),
+			progressLinear: $('.progress-linear'),
+			preloader: $('.preloader'),
+			rdNavbar: $('.rd-navbar'),
+			rdMailForm: $('.rd-mailform'),
+			rdInputLabel: $('.form-label'),
+			wow: $('.wow'),
+			parallaxJs: $(".parallax-scene-js"),
+			customWaypoints: $('[data-custom-scroll-to]'),
+		};
+
+	/**
+	 * @desc Check the element was been scrolled into the view
+	 * @param {object} elem - jQuery object
+	 * @return {boolean}
+	 */
+	function isScrolledIntoView(elem) {
+		if (isNoviBuilder) return true;
+		return elem.offset().top + elem.outerHeight() >= $window.scrollTop() && elem.offset().top <= $window.scrollTop() + $window.height();
+	}
+
+	/**
+	 * @desc Calls a function when element has been scrolled into the view
+	 * @param {object} element - jQuery object
+	 * @param {function} func - init function
+	 */
+	function lazyInit(element, func) {
+		var scrollHandler = function () {
+			if ((!element.hasClass('lazy-loaded') && (isScrolledIntoView(element)))) {
+				func.call();
+				element.addClass('lazy-loaded');
+			}
+		};
+
+		scrollHandler();
+		$window.on('scroll', scrollHandler);
+	}
+
+	// Initialize scripts that require a loaded page
+	$window.on('load', function () {
+		// Page loader & Page transition
+		if (plugins.preloader.length && !isNoviBuilder) {
+			pageTransition({
+				target: document.querySelector('.page'),
+				delay: 0,
+				duration: 500,
+				classIn: 'fadeIn',
+				classOut: 'fadeOut',
+				classActive: 'animated',
+				conditions: function (event, link) {
+					return link && !/(\#|callto:|tel:|mailto:|:\/\/)/.test(link) && !event.currentTarget.hasAttribute('data-lightgallery');
+				},
+				onTransitionStart: function (options) {
+					setTimeout(function () {
+						plugins.preloader.removeClass('loaded');
+					}, options.duration * .75);
+				},
+				onReady: function () {
+					plugins.preloader.addClass('loaded');
+					windowReady = true;
+				}
+			});
+		}
+
+		// Progress bar
+		if (plugins.progressLinear.length) {
+			for (var i = 0; i < plugins.progressLinear.length; i++) {
+				var
+					bar = $(plugins.progressLinear[i]),
+					initProgress = function () {
+						var
+							bar = $(this),
+							end = parseInt($(this).find('.progress-value').text(), 10);
+
+						if (!bar.hasClass("animated-first") && isScrolledIntoView(bar)) {
+							bar.find('.progress-bar-linear').css({ width: end + '%' });
+							bar.find('.progress-value').countTo({
+								refreshInterval: 40,
+								from: 0,
+								to: end,
+								speed: 1000
+							});
+							bar.addClass('animated-first');
+						}
+					};
+
+				$.proxy(initProgress, bar)();
+				$window.on("scroll", $.proxy(initProgress, bar));
+			}
+		}
+
+
+	});
+
+	// Initialize scripts that require a finished document
+	$(function () {
+		isNoviBuilder = window.xMode;
+
+		/**
+		 * @desc Initialize owl carousel plugin
+		 * @param {object} carousel - carousel jQuery object
+		 */
+		function initOwlCarousel(carousel) {
+			var
+				aliaces = ['-', '-sm-', '-md-', '-lg-', '-xl-', '-xxl-'],
+				values = [0, 576, 768, 992, 1200, 1600],
+				responsive = {};
+
+			for (var j = 0; j < values.length; j++) {
+				responsive[values[j]] = {};
+				for (var k = j; k >= -1; k--) {
+					if (!responsive[values[j]]['items'] && carousel.attr('data' + aliaces[k] + 'items')) {
+						responsive[values[j]]['items'] = k < 0 ? 1 : parseInt(carousel.attr('data' + aliaces[k] + 'items'), 10);
+					}
+					if (!responsive[values[j]]['stagePadding'] && responsive[values[j]]['stagePadding'] !== 0 && carousel.attr('data' + aliaces[k] + 'stage-padding')) {
+						responsive[values[j]]['stagePadding'] = k < 0 ? 0 : parseInt(carousel.attr('data' + aliaces[k] + 'stage-padding'), 10);
+					}
+					if (!responsive[values[j]]['margin'] && responsive[values[j]]['margin'] !== 0 && carousel.attr('data' + aliaces[k] + 'margin')) {
+						responsive[values[j]]['margin'] = k < 0 ? 30 : parseInt(carousel.attr('data' + aliaces[k] + 'margin'), 10);
+					}
+				}
+			}
+
+			carousel.owlCarousel({
+				autoplay: isNoviBuilder ? false : carousel.attr('data-autoplay') !== 'false',
+				autoplayTimeout: carousel.attr("data-autoplay") ? Number(carousel.attr("data-autoplay")) : 3000,
+				autoplayHoverPause: true,
+				loop: isNoviBuilder ? false : carousel.attr('data-loop') !== 'false',
+				items: 1,
+				center: carousel.attr('data-center') === 'true',
+				dotsContainer: carousel.attr('data-pagination-class') || false,
+				navContainer: carousel.attr('data-navigation-class') || false,
+				mouseDrag: isNoviBuilder ? false : carousel.attr('data-mouse-drag') !== 'false',
+				nav: carousel.attr('data-nav') === 'true',
+				dots: carousel.attr('data-dots') === 'true',
+				dotsEach: carousel.attr('data-dots-each') ? parseInt(carousel.attr('data-dots-each'), 10) : false,
+				animateIn: carousel.attr('data-animation-in') ? carousel.attr('data-animation-in') : false,
+				animateOut: carousel.attr('data-animation-out') ? carousel.attr('data-animation-out') : false,
+				responsive: responsive,
+				navText: carousel.attr('data-nav-text') ? $.parseJSON(carousel.attr('data-nav-text')) : [],
+				navClass: carousel.attr('data-nav-class') ? $.parseJSON(carousel.attr('data-nav-class')) : ['owl-prev', 'owl-next']
+			});
+		}
+
+		// UI To Top
+		if (isDesktop && !isNoviBuilder) {
+			$().UItoTop({
+				easingType: 'easeOutQuad',
+				containerClass: 'ui-to-top bx bx-chevron-up'
+			});
+		}
+
+		// RD Navbar
+		if (plugins.rdNavbar.length) {
+			var aliaces, i, j, len, value, values, responsiveNavbar;
+
+			aliaces = ["-", "-sm-", "-md-", "-lg-", "-xl-", "-xxl-"];
+			values = [0, 576, 768, 992, 1200, 1600];
+			responsiveNavbar = {};
+
+			for (i = j = 0, len = values.length; j < len; i = ++j) {
+				value = values[i];
+				if (!responsiveNavbar[values[i]]) {
+					responsiveNavbar[values[i]] = {};
+				}
+				if (plugins.rdNavbar.attr('data' + aliaces[i] + 'layout')) {
+					responsiveNavbar[values[i]].layout = plugins.rdNavbar.attr('data' + aliaces[i] + 'layout');
+				}
+				if (plugins.rdNavbar.attr('data' + aliaces[i] + 'device-layout')) {
+					responsiveNavbar[values[i]]['deviceLayout'] = plugins.rdNavbar.attr('data' + aliaces[i] + 'device-layout');
+				}
+				if (plugins.rdNavbar.attr('data' + aliaces[i] + 'hover-on')) {
+					responsiveNavbar[values[i]]['focusOnHover'] = plugins.rdNavbar.attr('data' + aliaces[i] + 'hover-on') === 'true';
+				}
+				if (plugins.rdNavbar.attr('data' + aliaces[i] + 'auto-height')) {
+					responsiveNavbar[values[i]]['autoHeight'] = plugins.rdNavbar.attr('data' + aliaces[i] + 'auto-height') === 'true';
+				}
+
+				if (isNoviBuilder) {
+					responsiveNavbar[values[i]]['stickUp'] = false;
+				} else if (plugins.rdNavbar.attr('data' + aliaces[i] + 'stick-up')) {
+					responsiveNavbar[values[i]]['stickUp'] = plugins.rdNavbar.attr('data' + aliaces[i] + 'stick-up') === 'true';
+				}
+
+				if (plugins.rdNavbar.attr('data' + aliaces[i] + 'stick-up-offset')) {
+					responsiveNavbar[values[i]]['stickUpOffset'] = plugins.rdNavbar.attr('data' + aliaces[i] + 'stick-up-offset');
+				}
+			}
+
+
+			plugins.rdNavbar.RDNavbar({
+				anchorNav: !isNoviBuilder,
+				stickUpClone: (plugins.rdNavbar.attr("data-stick-up-clone") && !isNoviBuilder) ? plugins.rdNavbar.attr("data-stick-up-clone") === 'true' : false,
+				responsive: responsiveNavbar,
+				callbacks: {
+					onStuck: function () {
+						var navbarSearch = this.$element.find('.rd-search input');
+
+						if (navbarSearch) {
+							navbarSearch.val('').trigger('propertychange');
+						}
+					},
+					onDropdownOver: function () {
+						return !isNoviBuilder;
+					},
+					onUnstuck: function () {
+						if (this.$clone === null)
+							return;
+
+						var navbarSearch = this.$clone.find('.rd-search input');
+
+						if (navbarSearch) {
+							navbarSearch.val('').trigger('propertychange');
+							navbarSearch.trigger('blur');
+						}
+
+					}
+				}
+			});
+
+
+			if (plugins.rdNavbar.attr("data-body-class")) {
+				document.body.className += ' ' + plugins.rdNavbar.attr("data-body-class");
+			}
+		}
+
+		// Owl carousel
+		if (plugins.owl.length) {
+			for (var i = 0; i < plugins.owl.length; i++) {
+				var carousel = $(plugins.owl[i]);
+				plugins.owl[i].owl = carousel;
+				initOwlCarousel(carousel);
+			}
+		}
+
+		// WOW
+		if ($html.hasClass("wow-animation") && plugins.wow.length && !isNoviBuilder && isDesktop) {
+			new WOW().init();
+		}
+
+
+		/**
+		 * Parallax JS
+		 * @description Creates a parallax effect between an array of layers, driving the motion from the gyroscope output of a smartdevice. If no gyroscope is available, the cursor position is used.
+		 */
+		if (plugins.parallaxJs.length) {
+			for (var i = 0; i < plugins.parallaxJs.length; i++) {
+				var scene = plugins.parallaxJs[i];
+				new Parallax(scene);
+			}
+		}
+
+		//Custom Waypoints
+		if (plugins.customWaypoints.length && !isNoviBuilder) {
+			var i;
+			for (i = 0; i < plugins.customWaypoints.length; i++) {
+				var $this = $(plugins.customWaypoints[i]);
+
+				$this.on('click', function (e) {
+					e.preventDefault();
+
+					$("body, html").stop().animate({
+						scrollTop: $("#" + $(this).attr('data-custom-scroll-to')).offset().top
+					}, 1000, function () {
+						$window.trigger("resize");
+					});
+				});
+			}
+		}
+
+	});
+}());
+
